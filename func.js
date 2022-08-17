57c5ce265fbfe0438006ef0d30cb6bddef268358
Pavan raju <pavankumarraju7@gmail.com>
2022-08-17 16:07:25 +0530

    function js

---
+++ func.js
@@ -0,0 +1,13 @@
+// functions in js
+
+
+
+function myFunction(p1, p2) {                  //keyword  function name   parameter
+  return p1 * p2;
+
+
+  
+}
+console.log(myFunction(5,3));                   //5,3 are arguments
+
+
